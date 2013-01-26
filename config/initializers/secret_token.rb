2395880a15bd8a78d84d88e99afdd02008d185bb
Michael Raimondi <zeno@pobox.com>
2013-01-26 16:32:38 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fivesecondcoder::Application.config.secret_token = '1377a553f1d8d7a798df0421b541736c791ec31668dfac5234ef1852c352bf1043c7e34cc261470713078e14bb2e0fdc1b8996fd6057e03df7f7af915ceb40e5'
